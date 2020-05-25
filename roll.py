7056ebc4b70f60ef3935d8a68af9a7cb83c96843
bigbeansjm18 <johnmitchelldev18@gmail.com>
2020-05-25 01:52:39 +0100

    basic if statements to embed functions within

---
+++ roll.py
@@ -0,0 +1,16 @@
+import sys
+
+print("Welcome to Dice Rolling Sim!")
+print("Type roll followed by your desired max number to roll the dice.")
+print("Or you can just type roll and it will use a regular 6 sided die.")
+
+firstAct = input("Type roll to start or quit to exit.")
+#roll function
+if firstAct == "roll":
+    print("rolled")
+#quit function
+elif firstAct == "quit":
+    print("Exiting DRS!")
+    sys.exit(0)
+else:
+    print("Type roll to start or quit to exit.")
