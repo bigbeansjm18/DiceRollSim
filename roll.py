4670fa7ddae0b53e3c92c0566e96c03610487931
bigbeansjm18 <johnmitchelldev18@gmail.com>
2020-05-25 02:09:40 +0100

    done basic 1-6 dice roll

--- roll.py
+++ roll.py
@@ -1,17 +1,14 @@
 import sys
+import random
 
 print("Welcome to Dice Rolling Sim!")
 print("Type roll followed by your desired max number to roll the dice.")
 print("Or you can just type roll and it will use a regular 6 sided die.")
-
+defaultMin = 1
+defaultMax = 6
 firstAct = input("Type roll to start or quit to exit.")
 #roll function
 if firstAct == "roll":
-    print("rolled")
+    print("The dice is rolling! Your number is...")
+    print(random.randint(defaultMin, defaultMax))
     
-#quit function
-elif firstAct == "quit":
-    print("Exiting DRS!")
-    sys.exit(0)
-else:
-    print("Type roll to start or quit to exit.")
